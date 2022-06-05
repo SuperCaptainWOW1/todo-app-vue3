694e9137adbb07b3a60386d4015f52443040cf8f
Vladimir <makhortov.dev@gmail.com>
2022-06-05 18:42:26 +0300

    Created todo app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img src="./assets/logo.png" alt="" />
+  <h1>Vue 3 Todo app</h1>
+  <form @submit.prevent="addNewTodo">
+    <label for="">New Todo</label>
+    <input type="text" name="newTodo" v-model="userInput" />
+    <button>Add New Todo</button>
+  </form>
+  <button @click="toggleAllDone">Check all done</button>
+  <button @click="removeAllTodos">Remove all</button>
+  <ul>
+    <li
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      @click="toggleDone(todo)"
+      class="todo"
+    >
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(index)">X</button>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const userInput = ref("");
+    const todos = ref([]);
+
+    const addNewTodo = () => {
+      todos.value.push({
+        id: Date.now(),
+        text: userInput.value,
+        done: false,
+      });
+
+      userInput.value = "";
+    };
+
+    const toggleDone = (todo) => {
+      todo.done = !todo.done;
+    };
+
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    const toggleAllDone = () => {
+      todos.value.forEach((todo) => (todo.done = true));
+    };
+
+    const removeAllTodos = () => {
+      todos.value = [];
+    };
+
+    return {
+      userInput,
+      todos,
+      addNewTodo,
+      toggleDone,
+      removeTodo,
+      toggleAllDone,
+      removeAllTodos,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+input,
+textarea,
+button,
+p,
+div,
+section,
+article,
+select {
+  display: "block";
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
+}
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
